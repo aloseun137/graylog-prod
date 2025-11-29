cc27a17c2e8c49651b294bf32743c1f3721b56c5
Seun Alo <oluwaseunoffice@gmail.com>
2025-11-29 09:37:22 -0500

    updated route file to allow internet access

--- graylog-terraform/route-tables.tf
+++ graylog-terraform/route-tables.tf
@@ -1,15 +1,36 @@
+### Public route table (Internet access)
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.graylog.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
+
+  tags = { Name = "public-rtb" }
 }
 
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+### Private route table (NAT Gateway)
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.graylog.id
+
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
+
+  tags = { Name = "private-rtb" }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
